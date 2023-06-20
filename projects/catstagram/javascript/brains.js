ebd3e7438e7d0393c1d4387845987238f0f2fb50
Avery Pellegrin <craddock017@gmail.com>
2023-06-20 04:26:28 +0000

    ss

---
+++ projects/catstagram/javascript/brains.js
@@ -0,0 +1,32 @@
+var counter1 = 1;
+var counter2 = 1;
+var counter3 = 1;
+var counter4 = 1;
+var counter5 = 1;
+var counter6 = 1;
+function likePhoto() {
+    document.getElementById("likeCount1").innerHTML = counter1;
+    counter1 = counter1 + 1;
+    console.log(counter1);
+}
+function likePhoto2() {
+    document.getElementById("likeCount2").innerHTML = counter2;
+    counter2 = counter2 + 1;
+    console.log(counter2);
+}
+function likePhoto3() {
+    document.getElementById("likeCount3").innerHTML = counter3;
+    counter3 = counter3 + 1;
+}
+function likePhoto4() {
+    document.getElementById("likeCount4").innerHTML = counter4;
+    counter4 = counter4 + 1;
+}
+function likePhoto5() {
+    document.getElementById("likeCount5").innerHTML = counter5;
+    counter5 = counter5 + 1;
+}
+function likePhoto6() {
+    document.getElementById("likeCount6").innerHTML = counter6;
+    counter6 = counter6 + 1;
+}
