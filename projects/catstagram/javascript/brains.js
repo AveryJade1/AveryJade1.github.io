d6b21f378f0f8fdbb7652bb9f166d17f13762834
Avery Pellegrin <craddock017@gmail.com>
2023-06-20 04:42:51 +0000

    done

--- projects/catstagram/javascript/brains.js
+++ projects/catstagram/javascript/brains.js
@@ -30,3 +30,13 @@ function likePhoto6() {
     document.getElementById("likeCount6").innerHTML = counter6;
     counter6 = counter6 + 1;
 }
+const user = {
+    name: Avery,
+    name_last: Pellegrin,
+    followers: Kylie, Joey, Ray, Amaya, Shayne,
+    profile_pic: "https://th.bing.com/th/id/OIP.8KVq591rXEaTEOwx0b9KIgHaE8?w=263&h=185&c=7&r=0&o=5&pid=1.7"
+}
+setTimeout(function(){ document.getElementById("name").innerHTML = user.name;}, 3000);
+ setTimeout(function(){ document.getElementById("name_last").innerHTML = user.name_last;}, 3000);
+ setTimeout(function(){ document.getElementById("followers").innerHTML = "Followers: " + user.followers.length;}, 3000);
+ setTimeout(function(){ $("#profile_pic").attr("src", user.profile_pic);}, 3000);
