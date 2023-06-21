03fa9330de82a3b9ff265afd9378c4ba5aa20ead
Avery Pellegrin <craddock017@gmail.com>
2023-06-21 20:44:29 +0000

    circles

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -17,7 +17,7 @@ var init = function (window) {
 
     // TODO 1 : Declare and initialize our variables
     var circle;
-    var circles;
+    var circles = [];
 
     // TODO 2 : Create a function that draws a circle
     function drawCircle() {
